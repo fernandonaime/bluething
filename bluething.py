c4ca82b45e0adcf82ba7000a3578a52840441206
nemashafernando <112611202+fernandonaime@users.noreply.github.com>
2024-01-28 11:50:26 +0530

    Update bluething.py

--- bluething.py
+++ bluething.py
@@ -198,9 +198,9 @@ def log_category(control):
 
 def control_or_date_log():
     try:
-        print(indent("""
+        print("""
     \033[91m|======================== Log Generation ========================|\033[0m
-    \033[3mFor the above configurations do you want a log by date or by control, hit no to skip\033[0m""", '    '))
+    \033[3mFor the above configurations do you want a log by date or by control, hit no to skip\033[0m""")
         choice = y_n_choice().lower()
         if choice == 'y' or choice == 'yes' or choice == '':
             choice = input("""
