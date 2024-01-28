1973aa818f54f5e21c29d4875032d44e78868fed
nemashafernando <112611202+fernandonaime@users.noreply.github.com>
2024-01-28 23:21:01 +0530

    Update bluething.py

--- bluething.py
+++ bluething.py
@@ -148,13 +148,14 @@ def control_or_date_log():
     try:
         print("""
     \033[91m|======================== Log Generation ========================|\033[0m
+    
     \033[3mFor the above configurations do you want a log by date or by control, hit no to skip\033[0m""")
         choice = y_n_choice().lower()
         if choice == 'y' or choice == 'yes' or choice == '':
             choice = input("""
     Enter your choice as an integer:
-    1) Log by date
-    2) Log by control
+    1: Log by date
+    2: Log by control
 
     Please enter the index of your choice: """)
             choice = int(choice)
